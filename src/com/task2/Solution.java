38397d871151504e299853de847a068648165f88
anhtuan161295 <anhtuan161295@gmail.com>
2022-12-09 12:12:05 +0700

    add sample code

--- src/com/task2/Solution.java
+++ src/com/task2/Solution.java
@@ -3,13 +3,37 @@ package com.task2;
 // https://leetcode.com/discuss/interview-question/2890446/Prepare-a-notification-message-(cropping-words-with-three-dots)
 public class Solution {
 
+    public String solutionV2(String message, int K) {
+        if (K > message.length()) return message;
+
+        char[] chars = message.toCharArray();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < K; i++) {
+            stringBuilder.append(chars[i]);
+        }
+
+        String temp = stringBuilder.toString().trim();
+        if (temp.lastIndexOf(" ") == -1) {
+            temp = "";
+        } else {
+            while (temp.length() + 4 > K) {
+                temp = temp.substring(0, temp.lastIndexOf(" "));
+            }
+        }
+
+        String result = temp + " ...";
+        result = result.trim();
+        return result;
+    }
+
     public String solution(String message, int K) {
         if (K > message.length()) return message;
 
         //trim the string to the maximum length
         String trimmedString = message.substring(0, K);
 
-        if (trimmedString.lastIndexOf(" ") == -1){
+        if (trimmedString.lastIndexOf(" ") == -1) {
             trimmedString = "";
         } else {
             trimmedString = trimmedString.substring(0, Math.min(trimmedString.length(), trimmedString.lastIndexOf(" ")));
@@ -27,13 +51,13 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String result = solution.solution("And now here is my secret", 15);
+        String result = solution.solutionV2("And now here is my secret", 15);
         System.out.println(result); // And now ...
-        String result2 = solution.solution("There is an animal with four legs", 15);
+        String result2 = solution.solutionV2("There is an animal with four legs", 15);
         System.out.println(result2); // There is an ...
-        String result3 = solution.solution("super dog", 4);
+        String result3 = solution.solutionV2("super dog", 4);
         System.out.println(result3); // ...
-        String result4 = solution.solution("how are you", 20);
+        String result4 = solution.solutionV2("how are you", 20);
         System.out.println(result4); // how are you
     }
 }
