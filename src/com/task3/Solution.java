128488cd1c335db421e0556c10bd0f09e7697be5
anhtuan161295 <anhtuan161295@gmail.com>
2022-12-13 22:35:39 +0700

    add sample code

--- src/com/task3/Solution.java
+++ src/com/task3/Solution.java
@@ -1,69 +1,75 @@
 package com.task3;
 
-import java.util.HashMap;
-import java.util.Map;
-
 // https://leetcode.com/discuss/interview-question/2890453/Number-of-non-empty-rectangular-areas
 public class Solution {
 
     public int solution(int[][] A) {
         // write your code in Java SE 8
-        int maxSum = Integer.MIN_VALUE;
-        int maxRow = A.length;
-        int maxCol = A[0].length;
-
-        for (int leftCol = 0; leftCol < maxCol; leftCol++) {
-
-            for (int rightCol = leftCol; rightCol < maxCol; rightCol++) {
-                int sum = 0;
-                for (int row = 0; row < maxRow; row++) {
-                    sum += A[row][rightCol];
-
-                    int tempSum = numSubmatrixSumTarget(A, sum);
-                    maxSum = Math.max(tempSum, maxSum);
-                }
-            }
-        }
-        return maxSum - maxRow + maxCol;
+        int result = matches(A);
+        return result;
     }
 
-    public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] matrixCol_sum = new int[m][n];
+    // get sum of the area by selecting row and column
+    // start array will have first element as start row, second element as end row
+    // end array will have first element as start column, second element as end column
+    public int calculateAreaSum(int[][] arr, int[] start, int[] end) {
+        int sum = 0;
+        for (int row = start[0]; row <= end[0]; row++)
+            for (int col = start[1]; col <= end[1]; col++)
+                sum += arr[row][col];
+        return sum;
+    }
 
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < m; i++) {
-                matrixCol_sum[i][j] = matrix[i][j];
-                if (i > 0) {
-                    matrixCol_sum[i][j] += matrixCol_sum[i - 1][j];
+    //
+    public int matches(int[][] arr) {
+        int maxRow = arr.length - 1;
+        int maxCol = arr[maxRow].length - 1;
+        int total = calculateAreaSum(arr, new int[]{0, 0}, new int[]{maxRow, maxCol});
+
+        // Impossible to have same inside and outside count
+        if (total % 2 != 0)
+            return 0;
+
+        int target = total / 2;
+        int matches = 0;
+
+        for (int rowEnd = 0; rowEnd < arr.length; rowEnd++) {
+
+            for (int colEnd = 0; colEnd < arr[rowEnd].length; colEnd++) {
+
+                for (int rowStart = 0; rowStart < arr.length; rowStart++) {
+
+                    for (int colStart = 0; colStart < arr[rowEnd].length; colStart++) {
+                        if (calculateAreaSum(arr, new int[]{rowStart, colStart}, new int[]{rowEnd, colEnd}) == target)
+                            matches++;
+                    }
                 }
             }
         }
 
-        int res = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = i; j < m; j++) {
-                int[] array = new int[n];
-                Map<Integer, Integer> map = new HashMap<>();
-                map.put(0, 1);
-                int sum = 0;
-                for (int k = 0; k < n; k++) {
-                    array[k] = matrixCol_sum[j][k] - (i > 0 ? matrixCol_sum[i - 1][k] : 0);
-                    sum += array[k];
-                    res += map.getOrDefault(sum - target, 0);
-                    map.put(sum, map.getOrDefault(sum, 0) + 1);
-                }
-            }
-        }
-
-
-        return res;
+        return matches;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int result = solution.solution(new int[][]{{5, -1}, {-3, 2}, {0, 4}});
-        System.out.println(result); // 0
+        int result = solution.solution(new int[][]{
+                {1, 1, -2},
+                {3, 2, 4},
+                {-1, -2, -2}
+        });
+        System.out.println(result); // 7
+
+        int result2 = solution.solution(new int[][]{
+                {1, 1, 2},
+                {2, 0, 0},
+        });
+        System.out.println(result2); // 3
+
+        int result3 = solution.solution(new int[][]{
+                {5, -1},
+                {-3, 2},
+                {0, 4}
+        });
+        System.out.println(result3); // 0
     }
 }
